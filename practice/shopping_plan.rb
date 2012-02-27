4974a7d1ff684d8c261923610ec1d019d326a117
Dawid Janczak <dawid.janczak@gmail.com>
2012-02-28 00:05:03 +0100

    Modified buying items so that not all items are bought at once when in a shop.

--- practice/shopping_plan.rb
+++ practice/shopping_plan.rb
@@ -1,5 +1,8 @@
 require 'ap'
 
+$DEBUG_PATH = []
+$EDGES = 0
+
 raise "Input filename should be given as script argument!" unless ARGV.size > 0
 
 class Shop
@@ -18,6 +21,10 @@ class Shop
     @x == another_shop.x && @y == another_shop.y
   end
 
+  def get_item_price(item)
+    @items[item]
+  end
+
   #def items_cost(item_list)
     #items_to_buy(item_list).values.inject(:+)
   #end
@@ -30,24 +37,53 @@ class Shop
     #!(item_list - items_to_buy(item_list)).empty?
   #end
 
-  def buy_items(item_list)
-    result = [0, [], false]
+  #def buy_items(item_list)
+    #result = [0, [], false]
 
-    @items.each do |item, price|
-      perishable = item.to_s.concat(?!).intern
-      has_perishable = item_list.include?(perishable)
-      if item_list.include?(item) || has_perishable
-        result[0] += price
-        if has_perishable
-          result[1] << perishable
-          result[2] = true
+    #@items.each do |item, price|
+      #perishable = item.to_s.concat(?!).intern
+      #has_perishable = item_list.include?(perishable)
+      #if item_list.include?(item) || has_perishable
+        #result[0] += price
+        #if has_perishable
+          #result[1] << perishable
+          #result[2] = true
+        #else
+          #result[1] << item
+        #end
+      #end
+    #end
+
+    #result
+  #end
+
+  def buy_items(item_list)
+    items_to_buy = items_to_buy(item_list).keys
+    transactions = []
+
+    flattened = []
+    1.upto(items_to_buy.size) { |i| flattened << items_to_buy.combination(i).to_a }
+    flattened.flatten!(1)
+
+    flattened.each do |combination|
+      combination_transaction = [0, [], false]
+      combination.each do |item|
+        combination_transaction[0] += @items[item]
+        unless item_list.include?(item)
+          combination_transaction[1] << item.to_s.concat(?!).intern
+          combination_transaction[2] = true
         else
-          result[1] << item
+          combination_transaction[1] << item
         end
       end
+      transactions << combination_transaction
     end
 
-    result
+    transactions
+    #items_to_buy.each do |item, price|
+      ##Item in item_list finishes with '!'
+      #is_perishable = item_list[item].nil?
+    #end
   end
 
   def to_s
@@ -70,16 +106,16 @@ class ShoppingPlan
     @gas_price, @items, @shops = gas_price, items, shops
     #@items_left = @items.dup
     #@shops_left = @shops.dup
-    p traverse(0, @@HOME, @shops.dup, @items.dup)
+    #p traverse(0, @@HOME, @shops.dup, @items.dup)
   end
 
   def get_min_cost
     "%.7f" % traverse(0, @@HOME, @shops.dup, @items.dup)
   end
 
-  #def each_shop_having(item)
-    #@shops.each { |shop| yield shop if shop.has_item?(item) }
-  #end
+  def each_shop_having(item)
+    @shops.each { |shop| yield shop if shop.has_item?(item) }
+  end
 
   #def cheapest_shop(current_location = @@HOME)
     #costs = @shops.map do |shop|
@@ -91,39 +127,65 @@ class ShoppingPlan
     #ap costs
   #end
 
+  def cheapest_shop(item)
+    each_shop_having(item).min { |s1, s2| s1.get_item_price(item) <=> s2.get_item_price(item) }
+  end
+
   def traverse(current_cost, current_location, shops_left, items_left, go_home = false)
-    ap "Current cost is #{current_cost}"
-    ap "Current location is #{current_location}"
-    ap "Shops left are #{shops_left}"
-    ap "Items left are #{items_left}"
-    ap "should go home? #{go_home}"
-    ap "Total cost when no items left: #{current_cost + cost(current_location)}" if items_left.empty?
-    #TODO what when shops_left is empty?
+    #ap "Current cost is #{current_cost}"
+    #ap "Current location is #{current_location}"
+    #ap "Shops left are #{shops_left}"
+    #ap "Items left are #{items_left}"
+    #ap "should go home? #{go_home}"
+    #ap "Total cost when no items left: #{current_cost + cost(current_location)}" if items_left.empty?
+    #$DEBUG_PATH << "Entering from #{current_location}, have #{items_left} items left to buy and paid #{current_cost}."
+    #$DEBUG_PATH << "Shall I go home? #{go_home}"
+    #$DEBUG_PATH << "It seems there are no items left, leaving." if items_left.empty?
     return current_cost + cost(current_location) if items_left.empty?
+    # When there are no shops left and we still have items, it is a wrong way for sure.
+    #$DEBUG_PATH << "No shops left!" if shops_left.empty?
+    return 1.0/0 if shops_left.empty?
+    #$EDGES += 1
 
     traversed = []
+
+    p "No more shops left" if shops_left.empty?
 
     if go_home
       cost = current_cost + cost(current_location, @@HOME)
       traversed << (current_cost + traverse(cost, @@HOME, shops_left.dup, items_left.dup))
     end
     shops_left.each do |shop|
-      ap "Going to shop #{shop}"
-      transaction = shop.buy_items(items_left)
-      next if transaction[0] == 0
+      #ap "Going to shop #{shop}"
+      transactions = shop.buy_items(items_left)
+      #ap "Transactions made in shop #{shop}: #{transactions}"
+      next if transactions.empty?
+
       shops = shops_left - [shop]
-      items = items_left - transaction[1]
-      #p "Cost is #{cost(current_location, shop)}"
-      cost = current_cost + transaction[0] + cost(current_location, shop)
-      p "Before calling traverse"
-      ap "--------------------------------------------------------"
-      traversed << traverse(cost, shop, shops, items)
-      p "After calling traverse"
-      p "Traversed: #{traversed}"
+      base_cost = current_cost + cost(current_location, shop)
+
+      transactions.each do |transaction|
+        items = items_left - transaction[1]
+        cost = base_cost + transaction[0]
+        #$DEBUG_PATH << "Transaction #{transaction} in shop #{shop}"
+        traversed << traverse(cost, shop, shops, items)
+      end
+
+      #next if transaction[0] == 0
+      #shops = shops_left - [shop]
+      #items = items_left - transaction[1]
+      ##p "Cost is #{cost(current_location, shop)}"
+      #cost = current_cost + transaction[0] + cost(current_location, shop)
+      #p "Before calling traverse"
+      #ap "--------------------------------------------------------"
+      #traversed << traverse(cost, shop, shops, items)
+      #p "After calling traverse"
+      #p "Traversed: #{traversed}"
     end
 
-    p "Traversed: #{traversed}"
-    traversed.min
+    #p "Traversed: #{traversed}"
+    #$DEBUG_PATH << "It seems the best cost for paying from #{current_location} is #{traversed.compact.min}"
+    traversed.compact.min
   end
 
   def cost(first, second = @@HOME)
@@ -144,6 +206,9 @@ File.open('output.txt', 'w') do |f|
     end
     plan = ShoppingPlan.new(gas_price, items, shops)
     f.puts("Case ##{tc_nr + 1}: #{plan.get_min_cost}")
+    puts "Case ##{tc_nr + 1}: #{plan.get_min_cost}"
+    #ap $DEBUG_PATH
+    #p "Nr of edges traversed: #{$EDGES}"
     p "#########################################"
     p "#########################################"
     p "#########################################"
