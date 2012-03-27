1639e0234abcb246a46d26c41833089a940d3203
Dawid Janczak <dawid.janczak@gmail.com>
2012-03-27 21:45:23 +0200

    Added tests for 8 given testcases and is_valid? method to Triangle class.

--- beta2008/triangle_trilemma/triangle_trilemma.rb
+++ beta2008/triangle_trilemma/triangle_trilemma.rb
@@ -5,8 +5,16 @@ class Point < Struct.new(:x, :y)
 end
 
 class Triangle < Struct.new(:p1, :p2, :p3)
+  def is_valid?
+    p1_p2_denom = p2.x - p1.x
+    p1_p3_denom = p3.x - p1.x
+
+    return false if p1_p2_denom == 0 && p1_p3_denom == 0
+    return true if p1_p2_denom == 0 || p1_p3_denom == 0
+
+    first_to_second = (p2.y - p1.y) / (p1_p2_denom)
+    first_to_third = (p3.y - p1.y) / (p1_p3_denom)
+    first_to_second != first_to_third
+  end
 end
 
-module TriangleTrilemma
-
-end
