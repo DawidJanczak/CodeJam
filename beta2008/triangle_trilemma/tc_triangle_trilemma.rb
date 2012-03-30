29053e08724af61dce3ba75e371bb53d305c6d0b
Dawid Janczak <dawid.janczak@gmail.com>
2012-03-30 23:53:12 +0200

    Modified TC so that it takes one screen instead of five (Ruby power). Triangle and point classes moved to triangle.rb so that the file input won't mess wiht tests (it didn't make sense anyway). Added out files to gitignore.

--- beta2008/triangle_trilemma/tc_triangle_trilemma.rb
+++ beta2008/triangle_trilemma/tc_triangle_trilemma.rb
@@ -1,77 +1,44 @@
-require_relative 'triangle_trilemma.rb'
+#!/usr/bin/ruby
+
+require_relative 'triangle.rb'
 require 'test/unit'
 
 class TestTriangle < Test::Unit::TestCase
   def setup
-    p1 = Point.new(0, 0)
-    p2 = Point.new(0, 4)
-    p3 = Point.new(1, 2)
-    @first = Triangle.new(p1, p2, p3)
+    points = [
+      [0, 0], [0, 4], [1, 2],
+      [1, 1], [1, 4], [3, 2],
+      [2, 2], [2, 4], [4, 3],
+      [3, 3], [3, 4], [5, 3],
+      [4, 4], [4, 5], [5, 6],
+      [5, 5], [5, 6], [6, 5],
+      [6, 6], [6, 7], [6, 8],
+      [7, 7], [7, 7], [7, 7],
+    ].map { |point| Point.new(*point) }
 
-    p4 = Point.new(1, 1)
-    p5 = Point.new(1, 4)
-    p6 = Point.new(3, 2)
-    @second = Triangle.new(p4, p5, p6)
-
-    p7 = Point.new(2, 2)
-    p8 = Point.new(2, 4)
-    p9 = Point.new(4, 3)
-    @third = Triangle.new(p7, p8, p9)
-
-    p10 = Point.new(3, 3)
-    p11 = Point.new(3, 4)
-    p12 = Point.new(5, 3)
-    @fourth = Triangle.new(p10, p11, p12)
-
-    p13 = Point.new(4, 4)
-    p14 = Point.new(4, 5)
-    p15 = Point.new(5, 6)
-    @fifth = Triangle.new(p13, p14, p15)
-
-    p16 = Point.new(5, 5)
-    p17 = Point.new(5, 6)
-    p18 = Point.new(6, 5)
-    @sixth = Triangle.new(p16, p17, p18)
-
-    p19 = Point.new(6, 6)
-    p20 = Point.new(6, 7)
-    p21 = Point.new(6, 8)
-    @seventh = Triangle.new(p19, p20, p21)
-
-    p22 = Point.new(7, 7)
-    @eighth = Triangle.new(p22, p22, p22)
+    @triangles = points.each_slice(3).inject([]) do |triangles, chunk|
+      triangles << Triangle.new(*chunk)
+    end
   end
 
   def test_is_valid
-    assert_equal(true, @first.is_valid?)
-    assert_equal(true, @second.is_valid?)
-    assert_equal(true, @third.is_valid?)
-    assert_equal(true, @fourth.is_valid?)
-    assert_equal(true, @fifth.is_valid?)
-    assert_equal(true, @sixth.is_valid?)
-    assert_equal(false, @seventh.is_valid?)
-    assert_equal(false, @eighth.is_valid?)
+    assertion_values = Array.new(6, true) << false << false
+    helper_tester(assertion_values, :is_valid?)
   end
 
   def test_lengths
-    assert_equal(:isosceles, @first.lengths_kind)
-    assert_equal(:scalene, @second.lengths_kind)
-    assert_equal(:isosceles, @third.lengths_kind)
-    assert_equal(:scalene, @fourth.lengths_kind)
-    assert_equal(:scalene, @fifth.lengths_kind)
-    assert_equal(:isosceles, @sixth.lengths_kind)
-    assert_nil(@seventh.lengths_kind)
-    assert_nil(@eighth.lengths_kind)
+    assertion_values = [:isosceles, :scalene, :isosceles, :scalene, :scalene, :isosceles, nil, nil]
+    helper_tester(assertion_values, :lengths_kind)
   end
 
   def test_angles
-    assert_equal(:obtuse, @first.angles_kind)
-    assert_equal(:acute, @second.angles_kind)
-    assert_equal(:acute, @third.angles_kind)
-    assert_equal(:right, @fourth.angles_kind)
-    assert_equal(:obtuse, @fifth.angles_kind)
-    assert_equal(:right, @sixth.angles_kind)
-    assert_nil(@seventh.angles_kind)
-    assert_nil(@eighth.angles_kind)
+    assertion_values = [:obtuse, :acute, :acute, :right, :obtuse, :right, nil, nil]
+    helper_tester(assertion_values, :angles_kind)
+  end
+  
+  def helper_tester(assertion_values, method_name)
+    assertion_values.zip(@triangles) do |assertion|
+      assert_equal(assertion[0], assertion[1].send(method_name))
+    end
   end
 end
--- beta2008/triangle_trilemma/triangle_trilemma.rb
+++ beta2008/triangle_trilemma/triangle_trilemma.rb
@@ -1,56 +1,6 @@
 #!/usr/bin/ruby
 
-require 'bigdecimal'
-
-class Point < Struct.new(:x, :y)
-  def -(another_point)
-    BigDecimal.new(((x - another_point.x) ** 2 + (y - another_point.y) ** 2)).sqrt(10)
-  end
-end
-
-class Triangle
-  @@PYTHAGOREAN_DIFFERENCE = { -1 => :obtuse, 0 => :right, 1 => :acute }
-
-  def initialize(p1, p2, p3)
-    @p1, @p2, @p3 = p1, p2, p3
-    @distances = [@p1 - @p2, @p1 - @p3, @p2 - @p3].sort
-  end
-
-  #Checking if triangle is valid can be done with checking its area
-  #using Heron's formula since we have all the distances between vertices.
-  #Of course triangle is also invalid if either two out of three points are the same.
-  def is_valid?
-    return false if @p1 == @p2 || @p1 == @p3 || @p2 == @p3
-    s = @distances.inject(0) { |sum, distance| sum + distance } / 2.0
-    (@distances.inject(1) { |multi, distance| multi * (s - distance) } * s) > 0
-  end
-
-  # Angles kind can be calculated based in Pythagorean Theorem.
-  # Type of the triangle is dependant on the equality of the
-  # two sides of the equation.
-  def angles_kind
-    return nil unless is_valid?
-    left_side = (@distances[0] ** 2 + @distances[1] ** 2).round
-    right_side = (@distances[2] ** 2).round
-    @@PYTHAGOREAN_DIFFERENCE[left_side <=> right_side]
-  end
-
-  # Having all distances all that needs to be done is checking
-  # whether there are any duplicate lengths to determine if
-  # the triangle is isosceles or scalene.
-  def lengths_kind
-    return nil unless is_valid?
-    @distances.length != @distances.uniq.length ? :isosceles : :scalene
-  end
-
-  def to_s
-    if is_valid?
-      "#{lengths_kind} #{angles_kind} triangle"
-    else
-      "not a triangle"
-    end
-  end
-end
+require_relative 'triangle.rb'
 
 contents = File.readlines(ARGV[0])
 contents.delete_at(0)
